61a20fe173ec1408fd07503c439d850e893be53f
Jason Joliet <jjoliet@asu.edu>
2018-04-14 21:22:17 -0700

    Working a bit more, had to add lib folder to build.xml

--- src/org/tempuri/WeatherClient.java
+++ src/org/tempuri/WeatherClient.java
@@ -1,17 +1,88 @@
 package org.tempuri;
 
+import java.io.IOException;
 import java.rmi.RemoteException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-public class WeatherClient {
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
-	public static void main(String[] args) {
-		IService client = new IServiceProxy();
+@SuppressWarnings("serial")
+public class WeatherClient extends HttpServlet {
+	private static IService client;
+	public void init(ServletConfig sc) throws ServletException {
+		super.init(sc);
+		client = new IServiceProxy();
+	}
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String contentType = "text/html";
+		response.setContentType(contentType);
+		response.setStatus(HttpServletResponse.SC_OK);
+		response.getWriter().print(client.getWeather("Denver", "Colorado"));
+	}
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//Using for verification currently.
+//		System.out.println(request.getParameter("state_names_for_weather"));
+//		System.out.println(request.getParameter("city_name_weather"));
+//		System.out.println(request.getParameter("hourly_check"));
+//		System.out.println(request.getParameter("Ten_day_check"));
+		String contentType = "text/html";
+		response.setContentType(contentType);
+		response.setStatus(HttpServletResponse.SC_OK);
+		//response.getWriter().print(client.getWeather_hourly("Denver", "Colorado", true));
+		String temp = client.getWeather_tenDays("Denver", "Colorado", true);
+		System.out.println("ok so far");
+		JsonParser parser = new JsonParser();
+		//JsonObject obj = parser.parse(temp).getAsJsonObject().get("current_observation").getAsJsonObject();
+		//System.out.println("Ok still... " + obj.get("weather")); Works for getting initial objects
+		
+//		JsonObject obj = parser.parse(temp).getAsJsonObject();
+//		JsonArray arr = obj.get("hourly_forecast").getAsJsonArray();
+//		System.out.println("Here you go " + arr.get(0).getAsJsonObject().get("FCTTIME").getAsJsonObject().get("pretty"));
+		
+		JsonObject obj = parser.parse(temp).getAsJsonObject().get("forecast").getAsJsonObject().get("simpleforecast").getAsJsonObject();
+		JsonArray arr = obj.get("forecastday").getAsJsonArray();
+		System.out.println("Here you go " + arr.get(0).getAsJsonObject());
+		
+		response.getWriter().print(temp);
+	}
+	
+	public void processRequest(HttpServletRequest request, HttpServletResponse response) {
+		//Process request Parameters
+		boolean hourly = request.getParameter("hourly_check").equalsIgnoreCase("on") ? true : false;
+		boolean tenDay = request.getParameter("Ten_day_check").equalsIgnoreCase("on") ? true : false;
+		String city_name = request.getParameter("city_name_weather");
+		String state_name = request.getParameter("state_names_for_weather");
+		String json_String ="";
+		JsonParser parser = new JsonParser();
+		//Determine which function to call. Defaults to "tenDay" if both hourly and ten day forecast are checked.
 		try {
-			System.out.println("get Weather: "  + client.getWeather("Denver", "Colorado"));
-		} catch (RemoteException e) {
+			if(tenDay) {
+				json_String = client.getWeather_tenDays(city_name, state_name, tenDay);				
+				JsonArray obj = parser.parse(json_String).getAsJsonObject().get("forecast").getAsJsonObject().get("simpleforecast").getAsJsonObject().get("forecastday").getAsJsonArray();
+				for(JsonElement el: obj) {
+					
+				}
+			}else if(hourly) {
+				json_String = client.getWeather_hourly(city_name, state_name, hourly);
+			}
+			else {
+				json_String = client.getWeather(city_name, state_name);
+			}
+		} catch (Exception e) {
+			//Bad handling, but for now, what I will have.
 			e.printStackTrace();
 		}
-
+		
+		
 	}
 
 }
--- src/org/tempuri/WeatherClientServlet.java
+++ src/org/tempuri/WeatherClientServlet.java
@@ -1,46 +1,41 @@
 package org.tempuri;
 
-import java.rmi.RemoteException;
-
-import javax.servlet.ServletConfig;
+import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 /**
- * 
- * @author Jason Joliet
- * Creates the weather client for WSDL @ http://vhost3.cs.rit.edu/weather/Service.svc?wsdl
- *
+ * Servlet implementation class WeatherClientServlet
  */
-@SuppressWarnings("serial")
-public class WeatherClientServlet extends HttpServlet{
-	
-	/**
-	 * Initialize the servlet
-	 */
-	public void init(ServletConfig sc) throws ServletException {
-		super.init(sc);
-		
-	}
-	/**
-	 * This happens on a get Request (Nothing)
-	 */
-	public void doGet(HttpServletRequest req, HttpServletResponse res) {
-		IService client = new IServiceProxy();
-		try {
-			System.out.println("get Weather: "  + client.getWeather("Denver", "Colorado"));
-		} catch (RemoteException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	
-		
-		
+@WebServlet("/WeatherClientServlet")
+public class WeatherClientServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public WeatherClientServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
 
-	
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
 
 }
